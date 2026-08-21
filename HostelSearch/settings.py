835d64397ea0c5a1254dccb069c5dc1998ca05a5
IBK-Tim <ibukunanifowose05@gmial.com>
2026-08-21 20:52:44 +0100

    Fix CSRF trusted origins

--- HostelSearch/settings.py
+++ HostelSearch/settings.py
@@ -136,3 +136,5 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 STATIC_URL  = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
+
+CSRF_TRUSTED_ORIGINS = ['https://hostelsearch-production.up.railway.app']
